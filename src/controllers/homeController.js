3fb0d32f41c54d69c1e7dc482fab8be2d1bc1a91
chalystacaramel <h1101241003@student.untan.ac.id>
2026-04-03 13:25:58 +0000

    UPDATE TAMPILAN

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,11 @@
 import { render } from "../config/viewEngine";
+import { prisma } from "../config/prisma";
 
 export const home = async (c) => {
+  const mahasiswa = await prisma.mahasiswa.findMany();
   const html = await render("home", {
     title: "Dashboard Bun MVC",
-    message: "Hello dari Bun + Tailwind",
+    mahasiswa: mahasiswa,
   });
   return c.html(html);
 };
